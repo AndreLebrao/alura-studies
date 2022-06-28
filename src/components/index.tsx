0dc0e2714925a7d04761d26c25c2dc70cebd2bfe
AndreLebrao <andledrao@gmail.com>
2022-06-28 12:13:22 -0300

    feature (components) : creating button

---
+++ src/components/index.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+// class component
+class Botao extends React.Component {
+    render(){
+        return(
+            <button>
+                Botão
+            </button>
+        )
+    }
+}
+export default Botao;
