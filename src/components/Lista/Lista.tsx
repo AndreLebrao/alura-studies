64b51bf366221b4db6a69a642a9a78bddd71a373
AndreLebrao <andledrao@gmail.com>
2022-07-03 10:17:35 -0300

    refactor (components) : extracting item component from list

--- src/components/Lista/Lista.tsx
+++ src/components/Lista/Lista.tsx
@@ -1,6 +1,7 @@
 import React from "react";
+import Item from "./Item/Item";
 
-import style from "./lista.module.scss"
+import style from "./Lista.module.scss"
 
 function Lista() {
   const tarefas = [
@@ -22,10 +23,9 @@ function Lista() {
       <h2>Estudos do dia</h2>
       <ul>
         {tarefas.map((item, index) => (
-          <li key={index} className={style.item} >
-            <h3>{item.tarefa}</h3>
-            <span>{item.tempo}</span>
-          </li>
+          <Item 
+          key={index}
+          {...item}/>
         ))}
       </ul>
     </aside>
