bc0679e898e225b312151c2d3249e4f862cd1688
AndreLebrao <andledrao@gmail.com>
2022-07-20 19:09:53 -0300

    feature (clock) : show selected activity time in clock

--- src/components/Cronometro/Cronometro.tsx
+++ src/components/Cronometro/Cronometro.tsx
@@ -1,13 +1,28 @@
 import Botao from "../Botao/Botao";
-import Relogio from './Relogio/Relogio';
-import style from "./Cronometro.module.scss"
+import Relogio from "./Relogio/Relogio";
+import style from "./Cronometro.module.scss";
+import { ITarefa } from "../../types/tarefas";
+import { useEffect, useState } from "react";
+import { tempoParaSegundos } from "../common/utils/time";
 
-export default function Cronometro() {
+interface props {
+  selecionado: ITarefa | undefined;
+}
+
+export default function Cronometro({ selecionado }: props) {
+  const [tempo, setTempo] = useState<number>();
+  useEffect(() => {
+    if (selecionado?.tempo) {
+      setTempo(tempoParaSegundos(selecionado.tempo));
+    }
+  }, [selecionado]);
   return (
     <div className={style.cronometro}>
       <p className={style.titulo}>
         Escolha um card e inicie o cronometro
-        <div className={style.relogioWrapper}><Relogio/></div>
+        <div className={style.relogioWrapper}>
+          <Relogio tempo={tempo}/>
+        </div>
         <Botao> Começar </Botao>
       </p>
     </div>
